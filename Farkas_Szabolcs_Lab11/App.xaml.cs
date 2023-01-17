d4a38d2ac1563e25370f96480917758a8c08b4e0
Szabolcs Tamas Farkas <tszabolcs26@yahoo.com>
2023-01-17 21:19:28 +0200

    Add project files.

---
+++ Farkas_Szabolcs_Lab11/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Farkas_Szabolcs_Lab11.Data;
+namespace Farkas_Szabolcs_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+
+            //InitializeComponent();
+
+            //MainPage = new AppShell();
+        }
+    }
+}
